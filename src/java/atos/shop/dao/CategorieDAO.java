4f482b9336598af74c62d0db88ddf7d6ee69c6f6
Administrateur <Administrateur@GSVK.formation.local>
2018-07-13 10:08:38 +0200

    version 1

--- src/java/atos/shop/dao/CategorieDAO.java
+++ src/java/atos/shop/dao/CategorieDAO.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package atos.shop.dao;
+package atos.shop.dao; 
 
 import atos.shop.entity.Categorie;
 import java.util.List;
